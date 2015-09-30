6d4e58604a3f8e8e566eaea3c1df9ee8f5305731
Aaron Steven White <aaronstevenwhite@gmail.com>
2015-09-30 16:13:57 -0400

    restructured directory further for packagification; added minimal content to README.md and began documenting

--- CHILDESPy/childes_xml_downloader.py
+++ CHILDESPy/childes_xml_downloader.py
@@ -1,3 +1,17 @@
+'''
+Download part or all of the CHILDES child-directed speech database as XML
+
+Exposes one class ``CHILDESDir``, which takes a remote corpus root and
+a local corpus root and directly copies either the entire file hierarchy
+associated with the corpus root (all corpora in the database) or a
+corpus named by the user
+
+Terminology: a root or subroot is a nonterminal node in the CHILDES file
+hierarchy that does not directly contain a node containing transcripts;
+a corpus is a non-terminal node in the hierarchy that a collection of
+does directly contain transcripts
+'''
+
 import os
 import re
 import nltk
@@ -10,73 +24,93 @@ from nltk.downloader import Downloader
 
 class CHILDESDir(object):
     
-    def __init__(self, url, local_path):
-        req = requests.get(url)
+    def __init__(self, remote_root, local_root):
+        self.remote_root = remote_root        
+        self.local_root = local_root
+        self.name = os.path.basename(local_root.strip('/')).lower()
+        
+        self.corpora = self._get_corpora(remote_children, local_children)
+
+    def _request_children(self):
+        '''
+        get location of children of remote root and construct pathnames
+        for both remote root and local root
+        '''
+        
+        req = requests.get(self.remote_root)
         soup = bs4.BeautifulSoup(req.text)
 
         anchors = soup.find_all('a', href=re.compile('^[A-Z]'))
 
-        corpus_urls = [os.path.join(url, anch['href']) for anch in anchors]
-        corpus_paths = [os.path.join(local_path, anch['href']) for anch in anchors]
+        remote_children = [os.path.join(self.remote_root, anch['href']) for anch in anchors]
+        local_children = [os.path.join(self.local_root, anch['href']) for anch in anchors]
+
+        return zip(remote_children, local_children)
         
-        self.local_path = local_path
-        self.name = os.path.basename(local_path.strip('/')).lower()
-
-        self.subcorpora = self._get_subcorpora(corpus_urls, corpus_paths)
-
-    def _get_subcorpora(self, corpus_urls, corpus_paths):
+    def _get_corpora(self):
+        '''
+        for each child of the remote root, create a CHILDESTranscript
+        if that child is a corpus or another CHILDESDir if it is a subroot
+        '''
+        
         children = {}
-
-        for url, path in zip(corpus_urls, corpus_paths):
-            if re.findall('\.zip$', url):
-                child = CHILDESTranscripts(url, self.local_path)
+        
+        for remote_root, path in self._request_children():
+            if re.findall('\.zip$', remote_root):
+                child = CHILDESCorpus(remote_root, self.local_root)
             else:
-                child = CHILDESDir(url, path)
+                child = CHILDESDir(remote_root, path)
 
             children[child.name] = child
 
         return children
 
-    def download_subcorpus(self, name):
+    def download_corpus(self, name):
+        '''download particular corpus in CHILDES'''
+        
         child_name = name.lower()
 
-        self.subcorpora[name].download()
+        self.corpora[name].download()
 
         print '{} from {} successfully downloaded and extracted'.format(child_name, self.name)
 
-    def download_subcorpora(self, corpus_names):
+    def download_corpora(self, corpus_names):
         for name in corpus_names:
-            self.download_subcorpus(name)
+            self.download_corpus(name)
 
     def download(self):
-        for corpus in self.subcorpora.values():
+        '''download all of CHILDES'''
+        
+        for corpus in self.corpora.values():
             corpus.download()
 
         print '{} successfully downloaded and extracted'.format(self.name)
 
-    def subcorpora_names(self):
-        return self.subcorpora.keys()
+    def corpora_names(self):
+        '''get names of corpora directly below this root'''
+        
+        return self.corpora.keys()
 
-class CHILDESTranscripts(object):
+class CHILDESCorpus(object):
 
-    def __init__(self, url, local_path):
-        self.url = url
-        self.local_path = local_path
+    def __init__(self, remote_root, local_root):
+        self.remote_root = remote_root
+        self.local_root = local_root
 
-        basename = os.path.basename(url)
+        basename = os.path.basename(remote_root)
         self.name = os.path.splitext(basename)[0].lower()
 
     def download(self):
-        req = requests.get(self.url)
+        '''download all transcripts from this corpus'''
+        
+        req = requests.get(self.remote_root)
         strio = StringIO.StringIO(req.content)
         
         zfile = zipfile.ZipFile(strio)
-        zfile.extractall(path=self.local_path)
+        zfile.extractall(path=self.local_root)
 
 
-def main():
-    childes_xml_url = 'http://childes.psy.cmu.edu/data-xml/'
-
+def main(childes_xml_remote_root='http://childes.psy.cmu.edu/data-xml/'):
     user_data_path = Downloader.default_download_dir(Downloader())
     childes_corpus_path = os.path.join(user_data_path, 'corpora/CHILDES/')
 
@@ -85,9 +119,8 @@ def main():
     except OSError:
         pass
 
-    return CHILDESDir(childes_xml_url, childes_corpus_path)
+    CHILDESDir(childes_xml_remote_root, childes_corpus_path).download()
 
 
 if __name__ == '__main__':
-    childes_root = main()
-    childes_root.download()
+    main()
---
+++ README.md
@@ -0,0 +1,3 @@
+# CHILDESPy
+
+CHILDESPy builds off of the NLTK corpus reader for the CHILDES database of child-direct speech. It includes modules for downloading the corpus as well as easily searching the corpuses various layers of annotation.
